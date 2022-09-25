9c31c1c3214907e42d205dae55a70ea245f80f12
proxer1982 <juanc.zorro@gmail.com>
2022-09-24 21:02:46 -0500

    vista crear movimiento

--- src/main/java/com/develpers3x2/thymeleaf/controller/TransactionController.java
+++ src/main/java/com/develpers3x2/thymeleaf/controller/TransactionController.java
@@ -1,12 +1,16 @@
 package com.develpers3x2.thymeleaf.controller;
 
 import com.develpers3x2.thymeleaf.entidad.Transaction;
+import com.develpers3x2.thymeleaf.entidad.Usuario;
 import com.develpers3x2.thymeleaf.service.ITransactionService;
+import com.develpers3x2.thymeleaf.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.logging.Level;
@@ -19,6 +23,8 @@ public class TransactionController {
 
     @Autowired
     private ITransactionService transactionService;
+    @Autowired
+    private IUsuarioService usuarioService;
 
     private final Logger LOG = Logger.getLogger("" + TransactionController.class);
 
@@ -37,6 +43,38 @@ public class TransactionController {
         model.addAttribute("total", total);
 
         return "movimientos/listar";
+    }
+    @GetMapping("/movements/modificar")
+    public String createTransaction(Model model){
+        LOG.log(Level.INFO, "createTransaction");
+
+        Transaction transaccion = new Transaction();
+
+        List<Usuario> usuarios = usuarioService.findAll();
+
+        titulo = "Datos de transacción";
+
+        model.addAttribute("titulo", titulo);
+        model.addAttribute("transaccion", transaccion);
+        model.addAttribute("usuarios", usuarios);
+
+        return "movimientos/modificar";
+
+    }
+    @PostMapping("/guardar")
+    public String saveTransaction(Transaction trans, BindingResult error, Model model){
+        LOG.log(Level.INFO, "saveTransaction");
+
+        if (error.hasErrors()){
+            System.out.println("Si hay errores");
+        }
+
+        trans.setEstado(true);
+        trans = transactionService.createTransaction(1,trans);
+
+        return "redirect:/movements/list";
+
+
     }
 
 
